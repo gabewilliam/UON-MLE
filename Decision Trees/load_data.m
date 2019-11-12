f1ea73ffc08fc729bcf9c708644bf1c8a163c70b
gabewilliam <gabe.william@hotmail.co.uk>
2019-11-12 10:20:38 +0000

    fixed y features & added shuffling

--- Decision Trees/load_data.m
+++ Decision Trees/load_data.m
@@ -3,13 +3,23 @@
 labels = load('label.csv');
 
 features_x = load('predx_for_classification.csv');
-features_y = load('predx_for_classification.csv');
+features_y = load('predy_for_classification.csv');
 
-labels_au1 = labels(:,1);
-labels_au2 = labels(:,2);
-labels_au3 = labels(:,3);
-labels_au4 = labels(:,4);
-labels_au5 = labels(:,5);
+%SHUFFLE DATA
 
-features_xy = cat(2, features_x, features_y);
+new_indices = randperm(size(labels,1));
+
+sh_features_x = features_x(new_indices,:);
+sh_features_y = features_y(new_indices,:);
+sh_labels = labels(new_indices,:);
+
+%RESHAPE DATA
+
+labels_au1 = sh_labels(:,1);
+labels_au2 = sh_labels(:,2);
+labels_au3 = sh_labels(:,3);
+labels_au4 = sh_labels(:,4);
+labels_au5 = sh_labels(:,5);
+
+features_xy = cat(2, sh_features_x, sh_features_y);
 
