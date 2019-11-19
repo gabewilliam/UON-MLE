d54843bb1a4a8fa098cdd4b63e316256c3f5bd7d
gabewilliam <gabe.william@hotmail.co.uk>
2019-11-19 12:12:34 +0000

    Add files via upload

---
+++ kfold_tree_trainer.m
@@ -0,0 +1,42 @@
+
+load_data;
+
+%number of folds to use
+n = 10;
+
+evaluations = zeros(n,3);
+
+for i = 1:n
+   
+    train_data =  cell2mat(features_xy_flds(1:end ~= i));
+    train_labels = cell2mat(labels_flds(1:end ~= i));
+    test_data = cell2mat(features_xy_flds(i));
+    test_labels = cell2mat(labels_flds(i));
+    
+    disp(size(train_data));
+    
+    %AU1 
+    train_labels = train_labels(:,1);
+    test_labels = test_labels(:,1);
+    
+    dTrees(i) = createDecisionTree(train_data, train_labels);
+    
+    [~,k] = size(test_labels);
+    
+    %Calculate evaluative measures
+    tree_outputs = zeros(k,1);
+    
+    for j = 1:k
+        tree_outputs(j)=classify(test_data(j,:),dTrees(i));
+    end
+    
+    [precision,recall,F1] = evaluation(tree_outputs,test_labels);
+    evaluations(i,1) = precision;
+    evaluations(i,2) = recall;
+    evaluations(i,3) = F1;
+    
+end
+
+%Save trees for later use
+save('trees', dTrees);
+save('eval', evaluations);
---
+++ load_data.m
@@ -0,0 +1,35 @@
+%LOAD DATA
+
+labels = load('label.csv');
+
+features_x = load('predx_for_classification.csv');
+features_y = load('predy_for_classification.csv');
+
+%SHUFFLE DATA
+
+new_indices = randperm(size(labels,1));
+
+sh_features_x = features_x(new_indices,:);
+sh_features_y = features_y(new_indices,:);
+sh_labels = labels(new_indices,:);
+
+%RESHAPE DATA
+
+labels_au1 = sh_labels(:,1);
+labels_au2 = sh_labels(:,2);
+labels_au3 = sh_labels(:,3);
+labels_au4 = sh_labels(:,4);
+labels_au5 = sh_labels(:,5);
+
+features_xy = cat(2, sh_features_x, sh_features_y);
+
+%Divide data into K folds
+K=10;
+
+%Cut data down to size N
+N=1000; 
+features_xy = features_xy(1:N, :);
+labels = labels(1:N,1);
+
+features_xy_flds = kdivide(features_xy, K);
+labels_flds = kdivide(labels, K);
