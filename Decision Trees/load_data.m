67dac6ec4bd22559844b6f6ab4e1d133cf676741
gabewilliam <gabe.william@hotmail.co.uk>
2019-11-19 12:49:15 +0000

    Add files via upload

--- Decision Trees/load_data.m
+++ Decision Trees/load_data.m
@@ -15,21 +15,21 @@ sh_labels = labels(new_indices,:);
 
 %RESHAPE DATA
 
-labels_au1 = sh_labels(:,1);
-labels_au2 = sh_labels(:,2);
-labels_au3 = sh_labels(:,3);
-labels_au4 = sh_labels(:,4);
-labels_au5 = sh_labels(:,5);
+%labels_au1 = sh_labels(:,1);
+%labels_au2 = sh_labels(:,2);
+%labels_au3 = sh_labels(:,3);
+%labels_au4 = sh_labels(:,4);
+%labels_au5 = sh_labels(:,5);
 
 features_xy = cat(2, sh_features_x, sh_features_y);
-
+%features_xy = cat(2, features_x, features_y);
 %Divide data into K folds
 K=10;
 
 %Cut data down to size N
-N=1000; 
+N=5000; 
 features_xy = features_xy(1:N, :);
-labels = labels(1:N,1);
+labels = sh_labels(1:N,1);
 
 features_xy_flds = kdivide(features_xy, K);
 labels_flds = kdivide(labels, K);
