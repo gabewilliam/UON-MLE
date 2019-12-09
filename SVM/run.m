7437049f78bd04a8ebf5c33feb6660e44199fcb1
Yisong Wang <44617885+longddd@users.noreply.github.com>
2019-12-09 09:15:54 +0800

    Delete run.m

--- SVM/run.m
+++
@@ -1,2 +0,0 @@
-
-Mdl = fitcsvm(Name,Value, 'KernelFunction','linear', 'BoxConstraint',1,'OptimizeHyperparameters','auto','HyperparameterOptimizationOptions',struct('AcquisitionFunctionName','expected-improvement-plus','ShowPlots',true));
